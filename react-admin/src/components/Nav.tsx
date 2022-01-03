ee265671f78d741cd10e31276a8350c744b85afc
EnricoGiovanno8 <genrico189@gmail.com>
2022-01-03 10:36:11 +0700

    Users Done

--- react-admin/src/components/Nav.tsx
+++ react-admin/src/components/Nav.tsx
@@ -16,7 +16,8 @@ const Nav = () => {
                     data.id,
                     data.first_name,
                     data.last_name,
-                    data.email
+                    data.email,
+                    data.role
                 ))
             }
         )()
